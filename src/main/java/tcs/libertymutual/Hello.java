4d80886ce698bef0146760cb3e849de0fd970573
abish <abish@Abisha>
2024-07-25 22:00:31 +0530

    Committing the program

---
+++ src/main/java/tcs/libertymutual/Hello.java
@@ -0,0 +1,14 @@
+package tcs.libertymutual;
+
+public class Hello {
+
+
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			String name = "Sree Kumar";
+			int output = name.length();
+			System.out.println(+output);
+
+		}
+}
